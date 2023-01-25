204e8b47a4f822288ce6ec9659431a1f24d88550
JeSala <JeSala@alasocast2.sabana.ice>
2023-01-25 13:32:43 -0600

    CREACION PROYECTO INICIAL

---
+++ src/main/java/com/AaronTarea/AaronTareaApplication.java
@@ -0,0 +1,13 @@
+package com.AaronTarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaronTareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaronTareaApplication.class, args);
+	}
+
+}
