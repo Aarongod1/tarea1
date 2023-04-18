31afb98cde0f34f5402399b5c8869b0b645bd5d4
Aaron <Aaron@DESKTOP-LN4JDIO>
2023-04-18 01:04:14 -0600

    clase 7

---
+++ src/main/java/com/AaronTarea/controller/ClienteController.java
@@ -0,0 +1,53 @@
+package com.AaronTarea.controller;
+
+import com.AaronTarea.domain.Cliente;
+import com.AaronTarea.service.ClienteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class ClienteController {
+
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping("/cliente/listado")
+    public String inicio(Model model) {
+
+//     var clientes = clienteService.getClientes();
+     var clientes = clienteService.getClientePorNombre("Ana");
+     model.addAttribute("clientes", clientes);
+
+        return "/cliente/listado";
+    }
+
+    @GetMapping("/cliente/nuevo")
+    public String nuevoCliente(Cliente cliente) {
+        return "/cliente/modificar";
+    }
+
+    @PostMapping("/cliente/guardar")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.save(cliente);
+        return "redirect:/cliente/listado";
+    }
+
+    @GetMapping("/cliente/modificar/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "/cliente/modificar";
+    }
+
+    @GetMapping("/cliente/eliminarCliente/{idCliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.delete(cliente);
+        return "redirect:/cliente/listado";
+    }
+}
